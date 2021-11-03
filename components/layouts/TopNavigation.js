acab0752ce711b0a940c42367e46983e4d782225
Evan Park <evan.y.park@gmail.com>
2021-11-03 01:00:28 -0400

    Enabling SVG Rendering through react-native-svg-transformer

--- components/layouts/TopNavigation.js
+++ components/layouts/TopNavigation.js
@@ -8,12 +8,14 @@ import {
     StyleSheet
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Arrow from '../../assets/icons/evericons/arrow-left.svg'
 
 const TopNavigationLayout = (props) => {
     const navigation = useNavigation();
     return (
         <SafeAreaView>
             <View style={styles.container}>
+                <Arrow width="50" height="50" color="black" />
                 <Button title='back' onPress={() => { navigation.goBack() }} />
             </View>
             <View>
